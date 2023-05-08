59638f54de578c29fd664f7a2dbea82d6c71835a
mtwomey <mtwomey@beakstar.com>
2023-05-08 00:29:21 -0500

    Fixup literate deft config

--- modules/beakstar/mod-deft/config.el
+++ modules/beakstar/mod-deft/config.el
@@ -1,32 +1,3 @@
-(after! deft
-  (set-evil-initial-state! 'deft-mode 'emacs)
-  (setq deft-directory "~/org-mode")
-  (setq deft-recursive t)
-  (setq deft-extensions '("txt" "org"))
-  (setq deft-use-filename-as-title t)
-  (setq deft-current-sort-method 'title)
+(defconst this-directory (file-name-directory (or load-file-name buffer-file-name)))
 
-  (map! :map deft-mode-map
-
-        :desc "next line"
-        :e "C-j" 'next-line
-
-        :desc "previous line"
-        :e "C-k" 'previous-line
-
-        :desc "scroll-page-down"
-        :e "C-d" 'evil-scroll-page-down
-
-        :desc "scroll-page-up"
-        :e "C-u" 'evil-scroll-page-up
-
-        :desc "leader-key"
-        :e "C-SPC" #'doom/leader
-
-        :desc "deft-filter-clear"
-        :e "C-<backspace>" #'deft-filter-clear
-
-        :desc "kill-this-buffer"
-        :e "C-q" #'kill-this-buffer)
-
-  (setq deft-auto-save-interval 0))
+(org-babel-load-file (concat this-directory "literate-config.org"))
