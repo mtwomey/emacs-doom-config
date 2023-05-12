f1d44ee28b9dcc46f9e87b44d04e7cf9da3c80d1
mtwomey <mtwomey@beakstar.com>
2023-05-12 13:39:51 -0500

    Tweak dirvish keymap

--- modules/beakstar/dirvish/config.el
+++ modules/beakstar/dirvish/config.el
@@ -15,44 +15,42 @@
 (after! dirvish
   (dirvish-override-dired-mode)
   (map! :map dirvish-mode-map
-      :desc "fuzzy search"
-      :nvi "s" #'dirvish-narrow
-      :desc "fuzzy search"
-      :nvi "/" #'dirvish-narrow
+        :desc "Search file names"
+        :nvi "/" #'dirvish-fd
 
-      :desc "dired find file"
-      :nvi "C-l" #'dired-find-file
-      :desc "dired find file"
-      :nvi "l" #'dired-find-file
+        :desc "dired find file"
+        :nvi "C-l" #'dired-find-file
+        :desc "dired find file"
+        :nvi "l" #'dired-find-file
 
-      :desc "dired up directory"
-      :nvi "C-h" #'dired-up-directory
-      :desc "dired up directory"
-      :nvi "h" #'dired-up-directory
+        :desc "dired up directory"
+        :nvi "C-h" #'dired-up-directory
+        :desc "dired up directory"
+        :nvi "h" #'dired-up-directory
 
-      :desc "dired previous line"
-      :nvi "C-k" #'dired-previous-line
-      :desc "dired previous line"
-      :nvi "k" #'dired-previous-line
+        :desc "dired previous line"
+        :nvi "C-k" #'dired-previous-line
+        :desc "dired previous line"
+        :nvi "k" #'dired-previous-line
 
-      :desc "dired next line"
-      :nvi "C-j" #'dired-next-line
-      :desc "dired next line"
-      :nvi "j" #'dired-next-line
+        :desc "dired next line"
+        :nvi "C-j" #'dired-next-line
+        :desc "dired next line"
+        :nvi "j" #'dired-next-line
 
-      :desc "Dirvish Dispatch"
-      :nvi "?" #'dirvish-dispatch
+        :desc "Dirvish Dispatch"
+        :nvi "?" #'dirvish-dispatch
 
-      :nvi "g" #'revert-buffer
+        :nvi "g" #'revert-buffer
 
-      :desc "Quick Access"
-      :nvi "a" #'dirvish-quick-access
+        :desc "Quick Access"
+        :nvi "a" #'dirvish-quick-access
 
-      :nvi "q" #'(lambda ()
-                 (interactive)
-                 (progn
-                   (dirvish-quit)
-                   (+dired/quit-all)))))
+        :nvi "q" #'(lambda ()
+                   (interactive)
+                   (progn
+                     (dirvish-quit)
+                     (+dired/quit-all)))))
 
 ;; Permissions problems when trying to use the mac trash based delete
 (setq delete-by-moving-to-trash nil)
