25d7d2a1b0ac3a324c16fa496deec349786f8f66
mtwomey <mtwomey@beakstar.com>
2023-05-14 23:05:49 -0500

    Switch to evil-cleverparens fork

--- modules/beakstar/evil-cleverparens/packages.el
+++ modules/beakstar/evil-cleverparens/packages.el
@@ -1,1 +1,4 @@
-(package! evil-cleverparens)
+;; patched version of evil-cleverparens
+;; https://clojurians.slack.com/archives/C01GE5PD249/p1666301564982439?thread_ts=1666289689.445859&cid=C01GE5PD249
+(package! evil-cleverparens
+ :recipe (:host github :repo "corasaurus-hex/evil-cleverparens" :files ("*.el")))
