c58daa21ff2f9158a4a62f3af59584e6101177e2
mtwomey <mtwomey@beakstar.com>
2023-06-19 01:40:21 -0500

    Revamp image-drawers to get rid of error
    Warning (bytecomp): ‘buffer-substring’ is an obsolete generalized variable.

--- init.el
+++ init.el
@@ -218,4 +218,5 @@
        evil-cleverparens
        brew
        org-download
+       image-drawers
        )
--- modules/beakstar/image-drawers/config.el
+++ modules/beakstar/image-drawers/config.el
@@ -8,11 +8,14 @@
                                      (when (string= drawer-name (org-element-property :drawer-name el))
                                        el))))))
     (cl-loop for drawer in image-drawers do
-             (setf (buffer-substring (org-element-property :begin drawer)
-                                     (- (org-element-property :end drawer) 1)) ;; Don't touch the character \n? right before the image or things get weird
-                   (concat save-string
-                           (buffer-substring (org-element-property :contents-begin drawer)
-                                             (- (org-element-property :contents-end drawer) 1))))))) ;; Don't touch the character \n? right before the image or things get weird
+             (let* ((drawer-begin (org-element-property :begin drawer))
+                    (drawer-end (org-element-property :end drawer))
+                    (drawer-contents-begin (org-element-property :contents-begin drawer))
+                    (drawer-contents-end (org-element-property :contents-end drawer))
+                    (drawer-contents (buffer-substring drawer-contents-begin (- drawer-contents-end 1))))
+               (goto-char drawer-begin)
+               (delete-char (- drawer-end drawer-begin 1))
+               (insert (concat save-string drawer-contents))))))
 
 (defun repack-image-drawers (&rest _)
   "Restore image drawers replaced using `unpack-image-drawers'."
@@ -24,13 +27,13 @@
                                         (when (string= "true" (nth 0 (org-element-property :attr_save el)))
                                           el))))))
     (cl-loop for paragraph in image-paragraphs do
-             (setf (buffer-substring (org-element-property :begin paragraph)
-                                     (- (org-element-property :contents-begin paragraph) 1))
-                   (concat ":" drawer-name ":\n"
-                           (buffer-substring (+ (length save-string) (org-element-property :begin paragraph))
-                                             (- (org-element-property :contents-begin paragraph) 1))
-                           "\n:END:")))))
-
+             (let* ((paragraph-begin (org-element-property :begin paragraph))
+                    (paragraph-contents-begin (org-element-property :contents-begin paragraph)))
+               (goto-char (- paragraph-contents-begin 1))
+               (insert "\n:END:")
+               (goto-char paragraph-begin)
+               (delete-char (length save-string))
+               (insert (concat ":" drawer-name ":\n"))))))
 
 (defun apply-with-image-drawers-unpacked (f &rest r)
   "Replace drawers named \"IMAGE_INFO\" with their contents, run the function,
