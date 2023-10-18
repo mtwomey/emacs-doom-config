050e373b9f4da7d7aac7abbdf9ff99ed1d7ceafe
mtwomey <mtwomey@beakstar.com>
2023-10-17 23:25:20 -0500

    Add add-hook-run-once macro

--- modules/beakstar/beakstar-utils/literate-init.el
+++ modules/beakstar/beakstar-utils/literate-init.el
@@ -6,3 +6,15 @@
   `(let ((load-path (cons ,directory load-path))
          (default-directory ,directory))
      ,@body))
+
+(defmacro add-hook-run-once (hook func)
+  "This takes a hook and a body. It defines a function and adds it to the hook
+using add-hook. The function will run the body and then remove itself from the
+hook with remove-hook. Finally the funciton will undefine itself with fmakunbound.
+This has the effect of a hook function that will run only one time"
+  (let ((unique-func-name (gensym "f")))
+       `(progn (defun ,unique-func-name ()
+                      (funcall ,func)
+                      (remove-hook ,hook ',unique-func-name)
+                      (fmakunbound ',unique-func-name))
+               (add-hook ,hook ',unique-func-name))))
